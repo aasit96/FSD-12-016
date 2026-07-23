36d083a7691a01766005f680367b85cf00b5a01f
aasit96 <aasitgupta9696@gmail.com>
2026-07-23 11:53:57 +0530

    initial commit

---
+++ LAB1/hello.js
@@ -0,0 +1,14 @@
+console.log("aasit");
+
+//write a function to take two number and return their sum
+function sum(a,b){
+    return a+b;
+}
+const add =(a,b)=>{
+    return a+b;
+
+};
+const add2=(a,b)=>a+b;
+
+console.log(sum(5,10));
+console.log(sum(10,1));
