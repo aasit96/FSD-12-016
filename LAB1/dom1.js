3ae36fb990f40bedcb1421b76ef2ddc7b4de2988
aasit96 <aasitgupta9696@gmail.com>
2026-07-30 11:47:58 +0530

    lab1 complete

---
+++ LAB1/dom1.js
@@ -0,0 +1,11 @@
+// Document object Model
+import { EventEmitter } from "events";
+
+const button = new EventEmitter();
+
+button.on("click",()=>{
+    console.log("Button clicked");
+
+});
+
+button.emit("click");
---
+++ LAB1/dom5.js
@@ -0,0 +1,32 @@
+import {EventEmitter} from "events";
+
+class DomClass extends EventEmitter{
+addEventListener(evenName, callback) {
+    this.on(evenName, callback);
+}
+  removeEventListener(eventName, callback){
+    this.off(eventName, callback);
+  }
+
+   dispatchEvent(eventName,eventData={}){
+    const event={
+        type:eventName,
+        timespam = new Data(),
+        ...eventData,
+    };
+    this.emit(eventName,event);
+   }
+}
+
+const button = new DomClass();
+const handleClick = (event)=>{
+    console.log(`Button clicked type: ${event.type} at ${event.timespam}`);
+};
+button.addEventListener('click',handleClick); 
+button.dispatchEvent("click",{
+    target:"submitBtn",
+});
+button.removeEventListener("click",handleClick); 
+button.dispatchEvent("click",{
+    target:"resetBtn",
+});
